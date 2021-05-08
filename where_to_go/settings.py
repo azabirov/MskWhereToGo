8917b2ffad2eebdaa8732aa35e8d25690ea08267
azabirov <amirzabirov@gmail.com>
2021-05-08 13:13:01 +0400

    Move data from template to view

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,11 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -7,3 +7,28 @@ from places.models import Place
 def test(request):
     places = Place.objects.all()
     return render(request, 'index.html', context={"places": places})
+
+
+def mainview(request):
+    data = []
+
+    for place in Place.objects.all():
+        data.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.coordinates.lng, place.coordinates.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.placeid,
+                "detailsUrl": "Hallo! Nothing here",
+            }
+        })
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": data
+    }
+
+    return render(request, 'index.html', context={'geojson': geojson})
