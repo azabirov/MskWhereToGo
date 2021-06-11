5bda2371b1f43746871148e7fbe765f6a659044d
azabirov <amirzabirov@gmail.com>
2021-06-12 01:02:22 +0400

    loadplace.py optimised

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -12,26 +12,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         url = options["url"]
-        link = requests.get(url)
-        json_data = link.json()
+        response = requests.get(url)
+        place_raw = response.json()
+        response.raise_for_status()
 
         place_, created = Place.objects.get_or_create(
-            placeid=slugify(json_data["title"]).replace("-", "_"),
-            title=json_data["title"],
-            description_long=json_data["description_long"],
-            description_short=json_data["description_short"],
+            title=place_raw["title"],
+            defaults={
+                'placeid': slugify(place_raw["title"]).replace("-", "_"),
+                'description_long': place_raw["description_long"],
+                'description_short': place_raw["description_short"],
+            }
         )
 
         coordinate = Coordinates.objects.get_or_create(
-            lng=json_data["coordinates"]["lng"],
-            lat=json_data["coordinates"]["lat"],
+            lng=place_raw["coordinates"]["lng"],
+            lat=place_raw["coordinates"]["lat"],
             place=place_,
         )
-        count = 0
-        for img_ in json_data["imgs"]:
-            count += 1
+
+        for index, img_ in enumerate(place_raw["imgs"]):
             image = Image.objects.get_or_create(
-                img=wget.download(img_, out=settings.MEDIA_ROOT).split("\\")[-1].split("/")[-1],
                 post=place_,
-                position=count,
+                position=index+1,
+                defaults={
+                    'img': wget.download(img_, out=settings.MEDIA_ROOT).split("\\")[-1].split("/")[-1],
+                }
             )
